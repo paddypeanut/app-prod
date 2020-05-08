c9b17b61e9cdfda5f3cb8c07ccba9d8519a7dd7c
Graham <paddypeanut@gmail.com>
2020-05-08 11:13:56 +0100

    added weekly jobs data

--- app/controllers/consignment_period_controller.rb
+++ app/controllers/consignment_period_controller.rb
@@ -8,7 +8,12 @@ class ConsignmentPeriodController < ApplicationController
 	end
 
 	def weekly
-
+		@consignments = Consignment.all
+		@userconsignments = @consignments.joins(:customer).includes(:user).where(user: session[:user_id])
+		@start = Date.today.beginning_of_week
+		@endDate = @start.end_of_week
+		@range = @start..@endDate
+		@results = @userconsignments.where("consignments.created_at" =>  @range)
 	end
 
 	def monthly
