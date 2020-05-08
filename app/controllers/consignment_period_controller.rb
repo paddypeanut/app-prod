54052e35bafeac3ad07ca7bee788cdc0c781e1d3
Graham <paddypeanut@gmail.com>
2020-05-08 11:30:49 +0100

    added functionality for daily,weekly,monthly consignments

--- app/controllers/consignment_period_controller.rb
+++ app/controllers/consignment_period_controller.rb
@@ -4,7 +4,10 @@ class ConsignmentPeriodController < ApplicationController
 	def daily
 		@consignments = Consignment.all
 		@userconsignments = @consignments.joins(:customer).includes(:user).where(user: session[:user_id])
-		@results = @userconsignments.where("consignments.created_at >= ?", Time.zone.now.beginning_of_day)
+		@start = Date.today.beginning_of_day
+		@endDate = @start.end_of_day
+		@range = @start..@endDate
+		@results = @userconsignments.where("consignments.created_at" => @range)
 	end
 
 	def weekly
@@ -17,6 +20,12 @@ class ConsignmentPeriodController < ApplicationController
 	end
 
 	def monthly
+		@consignments = Consignment.all
+		@userconsignments = @consignments.joins(:customer).includes(:user).where(user: session[:user_id])
+		@start = Date.today.beginning_of_month
+		@endDate = @start.end_of_month
+		@range = @start..@endDate
+		@results = @userconsignments.where("consignments.created_at" =>  @range)
 
 	end
 end
